4ec36f8201ce17a8d148eea974b3b277ea46b3d1
razzaksr <razzaksr@gmail.com>
2023-02-21 14:06:10 +0530

    Java Overview, Environment, Structure

---
+++ DLTE-Java/DLTE-Java-Java001/src/main/java/basic/dlte/javapart/App.java
@@ -0,0 +1,13 @@
+package basic.dlte.javapart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DLTE-Java/DLTE-Java-Java001/src/main/java/basic/dlte/javapart/Container.java
@@ -0,0 +1,9 @@
+package basic.dlte.javapart;
+
+public class Container {
+    public static void main(String[] args) {
+        System.out.println("BFSI is Evergreen domain");
+    }
+}
+
+
