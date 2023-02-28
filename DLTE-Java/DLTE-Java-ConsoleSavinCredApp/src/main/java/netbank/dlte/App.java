fcab73fca8cdb183835ba8bd8a401d8ea4dfd169
razzaksr <razzaksr@gmail.com>
2023-02-28 17:58:52 +0530

    SavinCredConsoleApp Build2 jar

--- DLTE-Java/DLTE-Java-ConsoleSavinCredApp/src/main/java/netbank/dlte/App.java
+++ DLTE-Java/DLTE-Java-ConsoleSavinCredApp/src/main/java/netbank/dlte/App.java
@@ -1,6 +1,8 @@
 package netbank.dlte;
 
+import java.io.*;
 import java.util.Date;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -9,9 +11,43 @@ import java.util.Scanner;
  */
 public class App 
 {
+    private static File file=new File("savincred.doc");
+    private static FileOutputStream fileOutputStream=null;
+    private static ObjectOutputStream objectOutputStream=null;
+    private static FileInputStream fileInputStream=null;
+    private static ObjectInputStream objectInputStream=null;
+
+    public static Map<String,KYC> readRecords(){
+        try{
+            fileInputStream =new FileInputStream(file);
+            objectInputStream=new ObjectInputStream(fileInputStream);
+
+            Map<String,KYC> buffer = (Map<String, KYC>)objectInputStream.readObject();
+
+            objectInputStream.close();
+            fileInputStream.close();
+            return buffer;
+        }
+        catch(IOException | ClassNotFoundException e){}
+        return null;
+    }
+
+    public static void writeRecords(Map<String,KYC> customers){
+        try{
+            fileOutputStream=new FileOutputStream(file);
+            objectOutputStream=new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(customers);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+        }
+        catch (IOException io){}
+    }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         SavinCredBank bank=new SavinCredBank();
+        //bank.setCustomers(readRecords());
         String processSelection="";
         int userSelection=0;
 
@@ -21,13 +57,16 @@ public class App
             userSelection=scanner.nextInt();
             switch (userSelection){
                 case 1:
+                    bank.setCustomers(readRecords());
                     bank.openAccount();
                     bank.report();
+                    writeRecords(bank.getCustomers());
                     break;
                 case 2:
                     System.out.println("-------------Welcome back --------------");
                     System.out.println("Enter the username");
                     String userName= scanner.next();
+                    bank.setCustomers(readRecords());
                     if(bank.getCustomers().containsKey(userName)){
                         KYC kyc=bank.getCustomers().get(userName);
                         System.out.println("Enter the password ");
@@ -39,21 +78,31 @@ public class App
                                 processSelection=scanner.next();
                                 switch (processSelection){
                                     case "deposit": case "Deposit":
+                                        bank.setCustomers(readRecords());
                                         System.out.println("Enter the amount to deposited");
                                         bank.deposit(kyc, scanner.nextInt(), new Date());
+                                        writeRecords(bank.getCustomers());
                                         break;
                                     case "withdraw": case "Withdraw":
+                                        bank.setCustomers(readRecords());
                                         System.out.println("Enter the amount to withdraw");
                                         bank.withdraw(kyc, scanner.nextInt(), new Date());
+                                        writeRecords(bank.getCustomers());
                                         break;
                                     case "Transfer": case "transfer":
+                                        bank.setCustomers(readRecords());
                                         System.out.println("Enter the amount to transfer and payee name");
                                         bank.transfer(kyc, scanner.nextInt(), scanner.next());
+                                        writeRecords(bank.getCustomers());
                                         break;
                                     case "statement": case "Statement":
+                                        bank.setCustomers(readRecords());
                                         bank.statement(kyc);
                                         break;
-                                    case "enquiry": case "Enquiry":System.out.println(kyc);break;
+                                    case "enquiry": case "Enquiry":
+                                        bank.setCustomers(readRecords());
+                                        System.out.println(kyc);
+                                        break;
                                     default:isLogged=false;
                                 }
                             }while(isLogged);
